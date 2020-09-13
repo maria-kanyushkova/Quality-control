ddace3bca4a6c8bf5589ed2d64f5c3f8d5b7fc42
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-09-13 20:40:22 +0300

    add lab1 Triangle with cli work

---
+++ src/main/java/lab1/Triangle.java
@@ -0,0 +1,65 @@
+package lab1;
+
+public class Triangle {
+    public static void main(String[] args) {
+        try {
+            System.out.println(checkTriangle(args));
+        } catch (Exception e) {
+            System.out.println("Ошибка");
+        }
+    }
+
+    public static String checkTriangle(String[] args) throws Exception {
+        if (isCorrectInput(args)) {
+            double[] doubleArray = new double[3];
+            doubleArray[0] = Double.parseDouble(args[0]);
+            doubleArray[1] = Double.parseDouble(args[1]);
+            doubleArray[2] = Double.parseDouble(args[2]);
+            if (isTriangleReal(doubleArray)) {
+                return typeOfTriangle(doubleArray);
+            } else {
+                throw new Exception();
+            }
+        } else {
+            throw new Exception();
+        }
+    }
+
+    public static boolean isCorrectInput(String[] args) throws Exception {
+        if (args.length != 3) {
+            throw new Exception();
+        }
+        return checkArgument(args[0]) && checkArgument(args[1]) && checkArgument(args[2]);
+    }
+
+    public static boolean isTriangleReal(double[] args) {
+        return args[0] + args[1] > args[2] && args[0] + args[2] > args[1] && args[1] + args[2] > args[0];
+    }
+
+    public static String typeOfTriangle(double[] args) {
+        if (args[0] == args[1] && args[0] == args[2]) {
+            return "Равносторонний";
+        } else if ((args[0] == args[1] && args[0] != args[2]) || (args[0] == args[2] && args[0] != args[1]) || (args[1] == args[2] && args[1] != args[0])) {
+            return "Равнобедренный";
+        } else {
+            return "Обычный";
+        }
+    }
+
+    private static boolean checkArgument(String arg) {
+        if (isDigit(arg)) {
+            double number = Double.parseDouble(arg);
+            return Double.MIN_VALUE <= number && number <= Double.MAX_VALUE;
+        }
+        return false;
+    }
+
+    private static boolean isDigit(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
